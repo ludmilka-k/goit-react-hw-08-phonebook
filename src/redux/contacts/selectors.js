ce23411fc6ef66158c3aac2cf0d141b058427dd3
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-11 16:00:18 +0300

    Add selectors

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -3,7 +3,6 @@ import {createSelector} from '@reduxjs/toolkit';
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
-export const selectFilter = state => state.filter;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilter], (contacts, filter) => {
