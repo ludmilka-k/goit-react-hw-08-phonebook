cdda5f9b7efc6aa003d49e71a518faa1132b86c9
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-11 18:56:46 +0300

    Add auth operations (thunks)

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const $instance = axios.create({
+  baseURL: 'https://connections-api.herokuapp.com',
+});
+
+export const setToken = token => {
+  $instance.defaults.headers['Authorization'] = `Bearer ${token}`;
+};
+
+export const clearToken = () => {
+  $instance.defaults.headers['Authorization'] = '';
+};
+
+export const registerUserThunk = createAsyncThunk(
+  'auth/register',
+  async (userData, thunkApi) => {
+    try {
+      const { data } = await $instance.post('/users/signup', userData);
+      setToken(data.token);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const loginUserThunk = createAsyncThunk(
+  'auth/login',
+  async (userData, thunkApi) => {
+    try {
+      const { data }= await $instance.post('/user/login', userData);
+      setToken(data.token);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const refreshUserThunk = createAsyncThunk(
+  'auth/refreshUser',
+  async ( _, thunkApi) => {
+    const state = thunkApi.getState();
+    const token = state.auth.token;
+
+    if (token === null) {
+      return thunkApi.rejectWithValue('Unable to fetch user');
+    }
+
+    try {
+      setToken(token);
+      const { data } = await $instance.get('/users/current');
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logoutUserThunk = createAsyncThunk(
+  'auth/logout',
+  async (_, thunkApi) => {
+    try {
+      const { data } = await $instance.post('/users/logout');
+      clearToken();
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
