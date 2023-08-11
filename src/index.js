7d1e5c8769dda5983457d985541026592ed5a52e
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-11 15:05:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react'
+import { persistor, store } from 'redux/store';
 import { App } from 'components/App';
-import {store} from './redux';
 import {theme} from './constants';
 import './index.css';
 
@@ -11,7 +12,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </React.StrictMode>
   </ThemeProvider>
