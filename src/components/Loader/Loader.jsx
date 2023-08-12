37db6a6dd44cb2212b65422fe03c355dadf47d37
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-12 09:03:28 +0300

    Add Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,19 @@
+import React from 'react';
+import { ThreeDots } from 'react-loader-spinner';
+import { LoaderStyled } from './Loader.styled';
 export const Loader = () => {
   return (
-    <></>
+    <LoaderStyled>
+      <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#4fa94d"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+      />
+    </LoaderStyled>
   );
 };
