dccf837dac5a9df545e77b01e3b19d3dfe006c4c
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-15 12:32:27 +0300

    Add Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import { AppBar } from '../AppBar';
+import { Footer } from '../Footer';
+import { Loader } from '../Loader';
+import { Wrapper } from './Layout.styled';
+
 export const Layout = () => {
   return (
-    <></>
+    <Wrapper>
+      <AppBar />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer />
+    </Wrapper>
   );
 };
