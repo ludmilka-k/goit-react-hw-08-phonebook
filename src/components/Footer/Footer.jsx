e1ed50457779a82048cb18a1910abd8f932efa50
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-15 09:27:36 +0300

    Add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,12 @@
+import  {StyledContainer} from './Footer.styled';
+
 export const Footer = () => {
   return (
-    <></>
+    <footer>
+      <StyledContainer>
+        <p>&#169; <a href="https://github.com/ludmilka-k/goit-react-hw-08-phonebook" target="_blank" rel="noreferrer"><b> Developed by Ludmila Kurochkina </b></a>
+          , 2023</p>
+      </StyledContainer>
+    </footer>
   );
 };
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const StyledContainer = styled.div`
+  margin-top: 24px;
+  margin-left: auto;
+  margin-right: auto;
+  color: ${props =>props.theme.colors.primaryText};
+  background: linear-gradient(270deg, #A5EDFF 0%, #FCFEFF 100%);
+`;
