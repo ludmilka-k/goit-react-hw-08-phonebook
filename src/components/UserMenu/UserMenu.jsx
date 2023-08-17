88fa70efe2b5711e3f357b911a874d560624c7e4
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-18 00:07:38 +0300

    Add UserMenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,5 +1,18 @@
+import { useDispatch } from 'react-redux';
+import {useSelector} from 'react-redux';
+import {logoutUserThunk} from '../../redux/auth/operations';
+import {selectUserData} from '../../redux/auth/selectors';
+import { Wrapper, Text, ButtonLogOut } from './UserMenu.styled';
+
 export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector(selectUserData);
   return (
-    <></>
+    <Wrapper>
+      <Text>Welcome to Phonebook {userData.name} </Text>
+      <ButtonLogOut type="button" onClick={() => dispatch(logoutUserThunk())}>
+        Logout
+      </ButtonLogOut>
+    </Wrapper>
   );
 };
---
+++ src/components/UserMenu/UserMenu.styled.js
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const Text = styled.p`
+  font-weight: 700;
+  color: #f08080;
+  font-size: 20px;
+`;
+
+export const ButtonLogOut = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  cursor: pointer;
+  font-size: 14px;
+  border: ${props => ` 1px solid ${props.theme.colors.grey}`};
+  border-radius: 4px;
+  padding: 12px;
+  background-color: ${props => props.theme.colors.white};
+  box-shadow: 0 1px 6px rgba(46, 47, 66, 0.08);
+
+  &:hover {
+    background-color: ${props => props.theme.colors.yellow};
+    border: ${props => ` 1px solid ${props.theme.colors.yellow}`};
+  }
+
+  &:active {
+    background-color: ${props => props.theme.colors.yellow};
+    border: ${props => ` 1px solid ${props.theme.colors.yellow}`};
+    box-shadow: none;
+    transform: translateY(1px);
+  }
+`;
