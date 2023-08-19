a4e38c9b7cce1361167ff9d9971317e02ee3c221
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-19 14:52:58 +0300

    Update styles

--- src/components/AuthNav/AuthNav.styled.js
+++ src/components/AuthNav/AuthNav.styled.js
@@ -1,19 +1,22 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+display: flex;
+  gap: 12px;
+`;
 export const StyledNavLink = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
   padding: 12px;
   font-weight: 700;
   color: ${props =>props.theme.colors.primaryText};
-  border: ${props => ` 1px solid ${props.theme.colors.grey}`};
+  border: ${props => ` 2px solid ${props.theme.colors.grey}`};
   border-radius: 4px;
   font-size: 20px;
 
   &:hover {
-    color: ${props =>props.theme.colors.yellow};
-    border: ${props => ` 1px solid ${props.theme.colors.yellow}`};
+    color: ${props =>props.theme.colors.blue};
+    border: ${props => ` 2px solid ${props.theme.colors.blue}`};
   }
 `
